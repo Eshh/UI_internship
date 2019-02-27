7020217067467af9a62080cb29d1268e183426a1
Eshwar <30635698+Eshh@users.noreply.github.com>
2019-02-27 16:38:13 +0530

    Add files via upload

---
+++ HelloWorld.js
@@ -0,0 +1,6 @@
+console.log("Hello World");
+//Comments are done in this way
+/*
+Multi line Comments
+are done in this way
+*/
---
+++ This_keyword.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+      <title></title>
+    </head>
+    <body>
+
+    </body>
+    <script type="text/javascript">
+      // This keyword used in a Method
+      var object = {
+        name : "Eshh",
+        Age : 21,
+        Gender : "Male"
+        Details : function()
+        {
+          return name +" "+ Age +" "+ Gender
+        }
+
+      }
+      object.Details()
+    </script>
+  </html>
